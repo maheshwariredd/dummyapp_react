6e6749b79314d420a48b32f02ba20ab017e644f0
maheshwariredd <gunreddyakihlreddy555@gmail.com>
2025-02-04 20:44:05 +0530

    sending react folders to the github

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+
+// function App() {
+//   let userName="Maheshwari"
+//   return (
+//     <div> 
+//      <h1>{userName}</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+// component composition
+
+import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Sidebar1 from './components/Sidebar1'
+import Sidebar2 from './components/Sidebar2'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Main/>
+      <div className = 'sidebar'>
+        <Sidebar1/>
+        <Sidebar2/>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+export default App
+
